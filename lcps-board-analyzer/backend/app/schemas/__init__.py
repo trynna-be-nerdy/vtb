88cc554c4efbb7f90ea4dcff40ad4b63a84c3ccf
trynna-be-nerdy <srivastkris06@gmail.com>
2026-05-09 22:59:31 -0400

    fix(lcps-board-analyzer): remove .env.example, config.json +57 more

--- lcps-board-analyzer/backend/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .meeting import MeetingCard, MeetingDetail
-from .agenda_item import AgendaItemOut
-from .category import CategoryOut, CategoryFeed
-
-__all__ = ["MeetingCard", "MeetingDetail", "AgendaItemOut", "CategoryOut", "CategoryFeed"]
